be5cd4ce3b2d9aa1858c06d322a107fd44f04326
MRV <sabudilovskiy@yandex.ru>
2024-03-28 12:58:28 +0000

    -

--- checks/unit/enum/enumerator_func.cpp
+++ checks/unit/enum/enumerator_func.cpp
@@ -3,13 +3,14 @@
 #include <userver/utest/utest.hpp>
 
 namespace enum_tests {
-    UOPENAPI_DECLARE_ENUM(Color2, int, red, green, blue);
+UOPENAPI_DECLARE_ENUM(Color2, int, red, green, blue);
 }  // namespace enum_tests
 using enum_tests::Color2;
 
 UTEST(EnumTests, BasicEnumeratorFunc) {
     using enum enum_tests::Color2;
-    userver::utils::TrivialBiMap biMap = uopenapi::create_enumerator_func<Color2>();
+    userver::utils::TrivialBiMap biMap =
+        uopenapi::create_enumerator_func<Color2>();
     EXPECT_EQ(biMap.TryFind(red), "red");
     EXPECT_EQ(biMap.TryFind(blue), "blue");
     EXPECT_EQ(biMap.TryFind(green), "green");
