4605dcf1653ddcd35492b9c5c94ce8e5bc4e5b8b
MRV <sabudilovskiy@yandex.ru>
2024-03-29 20:13:28 +0000

    -

--- checks/unit/enum/json.cpp
+++ checks/unit/enum/json.cpp
@@ -1,7 +1,8 @@
 #include <uopenapi/enum/declare.hpp>
-#include <uopenapi/json/string_like_parse.hpp>
-#include <uopenapi/json/string_like_serialize.hpp>
+#include <uopenapi/formats  //string_like_serialize.hpp>
+#include <uopenapi/formats/string_like_parse.hpp>
 #include <uopenapi/utils/converter/enum/all.hpp>
+#include <userver/formats/json.hpp>
 #include <userver/utest/utest.hpp>
 
 namespace enum_tests {
--- library/uopenapi/all.hpp
+++ library/uopenapi/all.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <uopenapi/enum/all.hpp>
+#include <uopenapi/formats/all.hpp>
 #include <uopenapi/http/all.hpp>
-#include <uopenapi/json/all.hpp>
 #include <uopenapi/pfr_extension/all.hpp>
 #include <uopenapi/reflective/all.hpp>
 #include <uopenapi/utils/all.hpp>
