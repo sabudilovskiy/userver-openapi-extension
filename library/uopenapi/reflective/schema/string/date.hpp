463aa7cc3c335933468f3a532cc4f7ec42748062
MRV <sabudilovskiy@yandex.ru>
2024-03-30 14:47:46 +0000

    -

---
+++ library/uopenapi/reflective/schema/string/date.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <uopenapi/reflective/schema/appender.hpp>
+#include <userver/utils/datetime/date.hpp>
+
+namespace uopenapi::reflective {
+    template <>
+    struct schema_appender<userver::utils::datetime::Date, none_requirements>{
+        template <none_requirements>
+        static void append(schema_view schemaView){
+            auto& cur = schemaView.cur_place;
+            cur["type"] = "string";
+            cur["format"] = "date";
+        }
+    };
+}
